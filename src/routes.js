736d8ee81ac5573ab787ca941e191f60f1f9ef03
Fabio Furtado <fabioassoni06@gmail.com>
2019-11-01 10:32:53 -0300

    Table "users" com seeder

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import StudentController from './app/controllers/StudentsController';
 const routes = new Router();
 
 routes.post('/students', StudentController.store);
+
 routes.put('/students/:id', StudentController.update);
 
 export default routes;
